284db853ba6c1f21dba7450de83dbd8627b6963a
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-02-07 16:24:03 +0530

    Added Data Types, Conditional Statements and Operators

---
+++ src/main/java/Assignments/Assignment2.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Assignment2 {
+    // High Tech Car
+    // DECLARE A VARIABLE - cameraVisibility = "LOW", "MEDIUM", "HIGH" (Accepted Values)
+    // Manual Driving Mode
+    // DECLARE A VARIABLE CALLED SPEED - If speed between 0 and 30 and visibility low
+    //                                 - then gear 1 and 2 allowed - output
+    //                                 - If speed between 31 - 60 and visibility Medium
+    //                                 - then gear 2 and 3 allowed - output
+    //                                 - if speed between 61 and 90 and visibility high
+    //                                 - then gear 3 and 4 allowed - output
+    // For Every Other condition - Output - high risk! Automatic Driving Mode
+}
