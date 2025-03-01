7530afa08e6cb97f71e329ca8bc096642a876de6
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-03-01 16:49:50 +0530

    Added Single and Multidimensional Arrays

---
+++ src/main/java/Assignments/Assignment5.java
@@ -0,0 +1,13 @@
+package Assignments;
+
+public class Assignment5 {
+    // FITNESS APP
+    // {"name", "age", "weight", "height", "medical_condition"}
+    // {"abc", "32", "78", "5.10", "none"}
+    // {"def", "40", "89", 5.7", "high bp"},
+    // {...}
+    // BMI calculate - Take formula from internet
+    // based on BMI and age , Recommend appropriate weight
+    // based on medical condition and age, recommend appropriate workout
+    // print in formatted way!
+}
